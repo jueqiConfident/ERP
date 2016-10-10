b51a28092faef30b091b52f1fb748951717bfb0b
Administrator <Administrator@USER-20150515LS>
2016-10-10 10:43:02 +0800

    cfd

--- src/testGit/HolleWorld.java
+++ src/testGit/HolleWorld.java
@@ -4,6 +4,7 @@ public class HolleWorld {
 	public static void main(String []args){
 		System.out.println("holleWorld");
 		System.out.println("holleWorld1");
-	
+		System.out.println();
+		System.out.println("vb r vb");
 	}
 }
